38a777debb7847ac7aab959bc10cc7634c5fae46
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2018-11-25 19:16:33 +0530

    Update classifier plot.py

--- classifier plot.py
+++ classifier plot.py
@@ -2,7 +2,7 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, ClassifierObject.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1, X2, ClassifierObject.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), #in place of "ClassifierObject", replace with the object created for the classification class
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
